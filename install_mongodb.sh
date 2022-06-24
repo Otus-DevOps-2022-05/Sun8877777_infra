2457f8827d62c5f1790fa12b6e4381351ba9b14c
Sun8877777 <an-cheremisin@yandex.ru>
2022-06-24 23:40:56 +0300

    HW04 fix test7

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-# for russia
-# sudo apt-get install -y mongodb
-# sudo systemctl start mongodb
-# sudo systemctl enable mongodb
+sudo apt-get install -y mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
