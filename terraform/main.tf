1d127efd8747b798582ab4b29c75fd2eec340ae4
Alexandr Cheremisin <an-cheremisin@yandex.ru>
2022-07-03 13:09:46 +0300

    HW06 Практика IaC с использованием Terraform2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.45.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "0.45.0"
+#     }
+#   }
+# }
 
 provider "yandex" {
   cloud_id  = var.cloud_id
